2b106e7a880bda315aac8212f292c2f95eb93579
Keith Benjamin <keith.i.benj@gmail.com>
2022-05-02 16:08:09 -0500

    Added completed update exercise

---
+++ update_exercises.sql
@@ -0,0 +1,17 @@
+use codeup_test_db;
+
+select 'All albums in your table' as 'info';
+select name from albums;
+update albums set sales = (sales * 10) ;
+select sales from albums;
+
+select 'All albums released before 1980' as 'info';
+select name from albums where release_date < 1980;
+update albums set release_date = 1800 where release_date < 1980;
+select release_date from albums;
+
+
+select 'All Michael Jackson albumbs' as 'info';
+select name from albums where artist = 'Michael Jackson';
+update albums set artist = 'Peter Jackson' where artist = 'Michael Jackson';
+select artist from albums;
