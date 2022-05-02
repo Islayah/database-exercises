5b9f66a8dfaf84d28f9995eca8d91d08ac54efe8
Keith Benjamin <keith.i.benj@gmail.com>
2022-05-02 16:21:51 -0500

    Updated the exercise as per instructions

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -1,20 +1,22 @@
 USE codeup_test_db;
 
+truncate albums;
+
 show tables;
 
 describe albums;
 
-INSERT INTO albums(artist, name, release_date, sales, genre)
-VALUES ('Michael Jackson', 'Thriller', 1982, 50.2, 'Pop, post-disco, funk, rock');
-
-insert into albums(artist, name, release_date, sales, genre)
-VALUES ('Whitney Houston', 'The bodyguard', 1992, 32.4, 'R&B, soul, pop, soundtrack');
-
-insert into albums(artist, name, release_date, sales, genre)
-VALUES ('Eagles', 'Their Greatest Hits (1971-1975)', 1976, 41.2, 'Country rock, soft rock, folk rock');
-
-insert into albums(artist, name, release_date, sales, genre)
-VALUES ('Eagles', 'Hotel California', 1976, 31.5, 'Soft rock');
+# INSERT INTO albums(artist, name, release_date, sales, genre)
+# VALUES ('Michael Jackson', 'Thriller', 1982, 50.2, 'Pop, post-disco, funk, rock');
+#
+# insert into albums(artist, name, release_date, sales, genre)
+# VALUES ('Whitney Houston', 'The bodyguard', 1992, 32.4, 'R&B, soul, pop, soundtrack');
+#
+# insert into albums(artist, name, release_date, sales, genre)
+# VALUES ('Eagles', 'Their Greatest Hits (1971-1975)', 1976, 41.2, 'Country rock, soft rock, folk rock');
+#
+# insert into albums(artist, name, release_date, sales, genre)
+# VALUES ('Eagles', 'Hotel California', 1976, 31.5, 'Soft rock');
 
 insert into albums(artist, name, release_date, sales, genre)
 VALUES ('Michael Jackson', 'Thriller', 1982, 50.2, 'Pop, post-disco, funk, rock'),
