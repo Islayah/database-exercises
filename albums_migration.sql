b48619e8b091e674c0c5379be4c7699c40ac25b5
Keith Benjamin <keith.i.benj@gmail.com>
2022-05-02 14:21:43 -0500

    Updated table values to work with the next exercise

--- albums_migration.sql
+++ albums_migration.sql
@@ -4,17 +4,32 @@ show databases;
 
 use codeup_test_db;
 
-create table albums (
+create table if not exists albums (
     id int unsigned not null auto_increment primary key,
     artist varchar(50) not null,
     name varchar(25) not null,
-    release_date date not null,
-    sales float not null,
-    genre varchar(20) not null
+    release_date int unsigned not null,
+    sales float unsigned not null,
+    genre varchar(20)
 );
 
 show tables;
 
 describe albums;
+
+drop table if exists albums;
+
+show tables;
+
+create table if not exists albums (
+                                      id int unsigned not null auto_increment primary key,
+                                      artist varchar(100) not null,
+                                      name varchar(100) not null,
+                                      release_date int unsigned not null,
+                                      sales float unsigned not null,
+                                      genre varchar(100)
+);
+
+
 
 SHOW CREATE TABLE albums;
